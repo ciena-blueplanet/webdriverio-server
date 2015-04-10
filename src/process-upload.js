722e165bb5ab1db771449a42085d311ff0b97f29
Peter Banka <peter.banka@gmail.com>
2015-04-10 09:57:24 -0700

    Adding the ability to specify an entrypoint. cleaning up output. making exec.sh more robust

--- src/process-upload.js
+++ src/process-upload.js
@@ -3,58 +3,42 @@
 var ns = {};
 var childProcess = require('child_process');
 
-/*
-var psTree = require('ps-tree');
-
-var kill = function (pid, signal, callback) {
-    signal   = signal || 'SIGKILL';
-    callback = callback || function () {};
-    var killTree = true;
-    if(killTree) {
-        psTree(pid, function (err, children) {
-            [pid].concat(
-                children.map(function (p) {
-                    return p.PID;
-                })
-            ).forEach(function (tpid) {
-                try { process.kill(tpid, signal) }
-                catch (ex) { }
-            });
-            callback();
-        });
-    } else {
-        try { process.kill(pid, signal) }
-        catch (ex) { }
-        callback();
-    }
+var strip = function (data) {
+    return data.toString().replace(/(\r\n|\n|\r)/gm,"");
 };
-*/
 
-ns.newFile = function (filename, res) {
+/**
+ * We have received a request to process a new file
+ * spawn a shell process to do all the operations and
+ * watch the output of that process. Then bundle up a json
+ * response for the requester.
+ * @param {String} filename - the name of the tar file that was uploaded
+ * @param {String} entryPoint - the URL to start testing
+ */
+ns.newFile = function (filename, entryPoint, res) {
     console.log('------------------- newFile');
     var info = [];
-    var error = [];
     var seconds = seconds = Math.floor(new Date().getTime() / 1000);
-    var child = childProcess.spawn('bash', ['./src/exec.sh', filename, seconds]);
+    var child = childProcess.spawn('bash', ['./src/exec.sh', filename, entryPoint, seconds]);
 
     child.stdout.on('data', function(data) {
-        console.log('stdout: ' + data);
+        console.log('stdout: ' + strip(data));
         info.push(data)
     });
     child.stderr.on('data', function(data) {
-        console.log('sterr: ' + data);
-        error.push(data)
+        console.log('sterr: ' + strip(data));
+        info.push(data)
     });
     child.on('exit', function(code) {
         console.log('closing code: ' + code);
         var output = {
             exitCode: code,
             info: info.join(''),
-            error: error.join(''),
             output: 'screenshots/' + seconds + '.tar',
         };
         res.send(JSON.stringify(output));
         res.end();
+        // TODO: setTimeout to delete the screenshots file in 30s
     });
 };
 
