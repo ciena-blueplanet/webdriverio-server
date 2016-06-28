ec89f05aeaee97d381baaff9a99d4a8b35cb9c89
Sam Pastoriza <pastorsj@rose-hulman.edu>
2016-06-27 17:08:06 -0700

    No need for mirage right now

--- mirage/factories/developer.js
+++
@@ -1,9 +0,0 @@
-import { Factory } from 'ember-cli-mirage'
-import generateToken from '../../app/utils/generateToken'
-
-export default Factory.extend({
-  username (i) {
-    return `user${i}`
-  },
-  token: generateToken(30)
-})
