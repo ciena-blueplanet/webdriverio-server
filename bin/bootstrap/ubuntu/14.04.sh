1a9cddbea3f8f869f5e56b213d1149fb5b04cafc
Sam Pastoriza <pastorsj@rose-hulman.edu>
2016-07-21 12:44:38 -0700

    Updated to latest version of webdriverio-client

--- bin/bootstrap/ubuntu/14.04.sh
+++ bin/bootstrap/ubuntu/14.04.sh
@@ -145,6 +145,13 @@ sudo mv nginx.conf /etc/nginx/nginx.conf
 # Reload with the new config
 sudo service nginx reload
 
+# Initialize MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod status
+
 # Start redis server
 sudo service redis-server restart
 
