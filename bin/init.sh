1ba6c0b77669e8fca8772cd1646a4707fc33cdbb
Peter Banka <pbanka@ciena.com>
2016-12-20 14:30:27 -0800

    Removing UI and updating docs

--- bin/init.sh
+++ bin/init.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# =================================================================================================================
-# Begin snippet taken from http://stackoverflow.com/a/246128 to figure out where the script lives
-# =================================================================================================================
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+if [ "$1" = "docker" ]; then
+  docker build -t webdriverio-server .
+else
+  # =================================================================================================================
+  # Begin snippet taken from http://stackoverflow.com/a/246128 to figure out where the script lives
+  # =================================================================================================================
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-# =================================================================================================================
-# End snippet from stackoverflow
-# =================================================================================================================
+  # =================================================================================================================
+  # End snippet from stackoverflow
+  # =================================================================================================================
 
-# start at the root of the project
-cd $DIR/..
+  # start at the root of the project
+  cd $DIR/..
 
-mkdir -p uploads/run
-mkdir -p screenshots
+  mkdir -p uploads/run
+  mkdir -p screenshots
 
-cd build
-npm install
-$NENV ./node_modules/.bin/webdriver-manager update --standalone
+  cd build
+  npm install
+  $NENV ./node_modules/.bin/webdriver-manager update --standalone
+fi
+
+
